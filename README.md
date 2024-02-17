f5c82d65501fbb2d997f4d8d1944114a8bed64fb
Greg Freitas <gregfreitas1997@gmail.com>
2024-02-17 15:29:18 -0500

    Initialized repository

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# leet-code
+# Leet Code Challenges
+
+The purpose of this repository is to contain solved Leet code challenges to give me an
+overview of my solved problems. This will help me track the types of problems I am working
+on overall.
---
+++ easy/lc_88_merge_sorted_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        if len(nums2) == 0:
+            return nums1 
+            
+            
